0cf0ed098ecfcd269ce0e34c9588daa314a080de
mr-tweaker <aniket.lamba@gmail.com>
2016-06-28 19:18:54 +0530

    Override the PRODUCT_NAME

--- full_cancro.mk
+++ full_cancro.mk
@@ -5,10 +5,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/cancro/cancro-vendor.mk)
 
-PRODUCT_NAME := full_cancro
+# Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := cancro
+PRODUCT_NAME := full_cancro
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := MI Cancro
+PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=cancro
